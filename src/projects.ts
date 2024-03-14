6081f3bbbe2c43eb78fc8e90a9c2ba697d1c2d1e
Chihab Jraoui <contact@chihabjraoui.com>
2024-03-14 08:47:53 +0000

    Create projects data

---
+++ src/projects.ts
@@ -0,0 +1,49 @@
+export const brazilProject = [{
+	title: "Manoa",
+	image: "/images/faried-anzyari-5vcygqehv0k-unsplash-scaled-65c4c1ce7eae6-1600x900.jpg",
+	category: "Avoided Unplanned Deforestation - REDD+",
+	location: "Brazil | Verra",
+	locationFlag: "https://cdn.jsdelivr.net/gh/umidbekk/react-flag-kit@1/assets/BR.svg"
+}];
+
+export const usaProject = [{
+	title: "Manoa",
+	image: "/images/faried-anzyari-5vcygqehv0k-unsplash-scaled-65c4c1ce7eae6-1600x900.jpg",
+	category: "Avoided Unplanned Deforestation - REDD+",
+	location: "United States | Verra",
+	locationFlag: "https://cdn.jsdelivr.net/gh/umidbekk/react-flag-kit@1/assets/US.svg"
+}];
+
+export const uaeProject = [{
+	title: "Borneo Peatlands",
+	image: "/images/faried-anzyari-5vcygqehv0k-unsplash-scaled-65c4c1ce7eae6-1600x900.jpg",
+	category: "Avoided Planned Deforestation",
+	location: "Saudi Arabia | Jeddah",
+	locationFlag: "https://cdn.jsdelivr.net/gh/umidbekk/react-flag-kit@1/assets/SA.svg"
+}];
+
+
+export const india = [{
+	title: "Uttar Pradesh Farmer Cooperatives",
+	image: "/images/faried-anzyari-5vcygqehv0k-unsplash-scaled-65c4c1ce7eae6-1600x900.jpg",
+	category: "Afforestation, Reforestation, and Revegetation",
+	location: "India | Verra",
+	locationFlag: "https://cdn.jsdelivr.net/gh/umidbekk/react-flag-kit@1/assets/IN.svg"
+}];
+
+export const defaultProjects = [
+	{
+		category: "Improved Forest Management",
+		title: "King County Parks",
+		location: "United States | Verra",
+		locationFlag: "https://cdn.jsdelivr.net/gh/umidbekk/react-flag-kit@1/assets/US.svg",
+		image: "/images/faried-anzyari-5vcygqehv0k-unsplash-scaled-65c4c1ce7eae6-1600x900.jpg",
+	},
+	{
+		category: "Afforestation, Reforestation, and Revegetation",
+		title: "Uttar Pradesh Farmer Cooperatives",
+		location: "India | Verra",
+		locationFlag: "https://cdn.jsdelivr.net/gh/umidbekk/react-flag-kit@1/assets/IN.svg",
+		image: "/images/faried-anzyari-5vcygqehv0k-unsplash-scaled-65c4c1ce7eae6-1600x900.jpg",
+	},
+];
