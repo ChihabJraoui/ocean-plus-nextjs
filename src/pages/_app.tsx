06a23ee733ebf92f73124e21c2ee5e7afff212f6
Chihab Jraoui <contact@chihabjraoui.com>
2024-03-21 21:33:15 +0000

    Integrate primereact library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "primereact/resources/themes/lara-light-blue/theme.css";
 import "../../public/css/global.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
