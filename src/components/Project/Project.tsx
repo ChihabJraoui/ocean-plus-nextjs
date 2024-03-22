f5b7fac77f4eb7f19809cec60c8a7b4c43f0b9ba
Chihab Jraoui <contact@chihabjraoui.com>
2024-03-22 12:49:59 +0000

    Optimize Project component

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {CSSProperties} from "react";
 import styles from "./Project.module.scss";
+import {useRouter} from "next/router";
 
 const spanStyle = {
 	boxSizing: "border-box",
@@ -37,16 +38,20 @@ const imgStyle = {
 export default function Project(props) {
 
 	const { data } = props;
+
+	const router = useRouter();
+
 	return (
-		<div className={styles["css-knybk"]}>
+		<div className={styles["project-card"]}>
 			<span style={spanStyle}>
-				<img src="./images/faried-anzyari-5vcygqehv0k-unsplash-scaled-65c4c1ce7eae6-1600x900.jpg"
+				<img src="/images/faried-anzyari-5vcygqehv0k-unsplash-scaled-65c4c1ce7eae6-1600x900.jpg"
 						 decoding="async"
 						 data-nimg="fill"
 						 className="css-1nf0x2k"
 						 style={imgStyle}
 						 sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 33vw" />
 			</span>
+
 			<div className={styles["css-qwl8h7"]}>
 				<div className={styles["css-wh3fb4"]}>
 					<div className={styles["css-1tsbzs"]}>
@@ -54,12 +59,15 @@ export default function Project(props) {
 							<p className={"chakra-text " + styles["css-cimxt8"]}></p>
 							<h5 className={"chakra-heading " + styles["css-1j0nf7q"]}></h5>
 							<p className={"chakra-text " + styles["css-1m942b8"]}>
-								<img className={"chakra-icon " + styles["css-mblvt0"]} alt="BR" width="24" height="24"
-										 src={`https://cdn.jsdelivr.net/gh/umidbekk/react-flag-kit@1/assets/${data.country.Initials}.svg`} />{data.country.Name} | {data.name}</p>
+								<img className={"chakra-icon " + styles["css-mblvt0"]} alt="BR" width="28" height="24"
+										 src={`https://cdn.jsdelivr.net/gh/umidbekk/react-flag-kit@1/assets/${data.country.countryCode}.svg`} />{data.country.name} | {data.name}</p>
 						</div>
 					</div>
 					<div className={styles["css-zwhd7k"]}>
-						<button type="button" className={"chakra-button " + styles["css-mwecow"]} role="button" onClick={() => window.location.href = `./wizard/${data.name}`}>Support now</button>
+						<button type="button"
+										role="button"
+										className={styles["support-button"]}
+										onClick={() => router.push(`./wizard/${data.name}`)}>Support now</button>
 					</div>
 				</div>
 			</div>
